55f2e89578e3c7e9e3cfe5d24a00f67aaf9f13b8
Mohammed Hamed <13mohammedhamed@gmail.com>
2024-10-02 20:09:17 +0300

    Time: 1372 ms (8.48%), Space: 0B (100.00%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,8 @@
+SELECT E.employee_id, E.name, 
+       COUNT(M.reports_to) AS reports_count,                                      ROUND(AVG(M.age*1.00), 0) AS average_age
+       
+FROM Employees E
+JOIN Employees M
+ON E.employee_id = M.reports_to
+GROUP BY E.employee_id, E.name
+ORDER BY E.employee_id
